6a4e1b24940d571cf1845bf1a9569cd70d23e06c
qufeichen <qufei.chen@gmail.com>
2016-03-28 19:43:08 -0400

    changed table and attribute names
    Table names changed to work with rails, and all primary key “id”
    attributes renamed to just “id” (to match what is created in the db)

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 --All the schema for the Movie Recommendation system
 
 CREATE TABLE users( -- view
-  user_id SERIAL UNIQUE,
+  id SERIAL UNIQUE,
   email varchar(255) UNIQUE,
   password varchar(255) NOT NULL,
   first_name varchar(255) NOT NULL,
@@ -11,44 +11,49 @@ CREATE TABLE users( -- view
   province varchar(255),
   country varchar(255),
   type_id int,
-  PRIMARY KEY (user_id),
+  PRIMARY KEY (id),
   FOREIGN KEY (type_id) REFERENCES user_types(type_id)
 );
 
 CREATE TABLE profiles( -- view
+  id SERIAL UNIQUE, 
   user_id int,
   age_range varchar(255) CHECK (age_range in ('0-13','14-17','18-20','20-30','30-40','40-50','60-70','80-90','100+')),
   gender char(1) CHECK (gender in ('M','F')),
   occupation varchar(255),
+  PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE user_types(
-  type_id SERIAL UNIQUE,
+CREATE TABLE user_types( -- model
+  id SERIAL UNIQUE,
   type_name varchar(255) UNIQUE,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE devices( -- do we need a view for devices?
-  device_id SERIAL UNIQUE,
+CREATE TABLE devices( -- model
+  id SERIAL UNIQUE,
   device_name varchar(255),
-  PRIMARY KEY (device_id)
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE devices_used(
+CREATE TABLE used_devices( --changed name (from devices_used) to match naming system --model
+  id SERIAL UNIQUE,
   user_id int NOT NULL,
   device_id int NOT NULL,
+  PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
   FOREIGN KEY (device_id) REFERENCES devices(device_id)
 );
 
-CREATE TABLE topics(
-  topic_id SERIAL UNIQUE,
+CREATE TABLE topics( -- view
+  id SERIAL UNIQUE,
   genre varchar(255),
-  PRIMARY KEY (topic_id)
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE movies( -- view
-  movie_id SERIAL UNIQUE,
+  id SERIAL UNIQUE,
   name varchar(255) NOT NULL,
   description varchar(255) NOT NULL,
   date_released date,
@@ -58,22 +63,26 @@ CREATE TABLE movies( -- view
   dubbed char(1) CHECK (dubbed in ('Y','N')),
   country varchar(255),
   age_rating varchar(255),
-  PRIMARY KEY (movie_id)
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE watches(
+CREATE TABLE movie_ratings( -- view
+  id SERIAL UNIQUE,
   user_id int,
   movie_id int,
   date_watched date,
   user_rating int CHECK (user_rating BETWEEN 0 AND 10),
   review varchar(255),
+  PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE
 );
 
-CREATE TABLE movie_topics(
+CREATE TABLE movie_topics( --model
+  id SERIAL UNIQUE,
   movie_id int,
   topic_id int,
+  PRIMARY KEY (id),
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE
   FOREIGN KEY (topic_id) REFERENCES topics(topic_id) ON DELETE CASCADE,
 );
@@ -88,67 +97,77 @@ CREATE TABLE actors( --view
   PRIMARY KEY (actor_id)
 );
 
-CREATE TABLE casting_types(
-  cast_id SERIAL UNIQUE,
+CREATE TABLE casting_types( -- model
+  id SERIAL UNIQUE,
   cast_type varchar(255) UNIQUE,
-  PRIMARY KEY (cast_id)
+  PRIMARY KEY (id)
 )
 
-CREATE TABLE roles(
-  role_id SERIAL UNIQUE,
+CREATE TABLE roles( -- model
+  id SERIAL UNIQUE,
   character_name varchar(255),
   PRIMARY KEY (role_id)
 );
 
-CREATE TABLE movie_casts(
+CREATE TABLE movie_casts( -- model
+  id SERIAL UNIQUE,
   movie_id int,
   actor_id int,
-  cast_id int,
+  casting_type_id int,
+  PRIMARY KEY (id),
   FOREIGN KEY (actor_id) REFERENCES actors(actor_id) ON DELETE SET NULL,
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE,
   FOREIGN KEY (cast_id) REFERENCES casting_types(cast_id) ON DELETE SET NULL
 );
 
-CREATE TABLE actor_roles(
+CREATE TABLE actor_roles( -- model
+  id SERIAL UNIQUE,
   actor_id int,
   role_id int,
+  PRIMARY KEY (id),
   FOREIGN KEY (actor_id) REFERENCES actors(actor_id) ON DELETE CASCADE,
   FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
 );
 
-CREATE TABLE movie_roles(
+CREATE TABLE movie_roles( -- model
+  id SERIAL UNIQUE,
   movie_id int,
   role_id int,
+  PRIMARY KEY (id),
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE,
   FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
 );
 
 CREATE TABLE directors( -- view
-  director_id SERIAL UNIQUE,
+  id SERIAL UNIQUE,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL,
   date_of_birth varchar(255),
   place_of_birth varchar(255),
-  PRIMARY KEY (director_id)
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE directs(
+CREATE TABLE directs( -- model
+  id SERIAL UNIQUE,
   director_id int,
   movie_id int,
+  PRIMARY KEY (id),
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE,
   FOREIGN KEY (director_id) REFERENCES directors(director_id) ON DELETE CASCADE
 );
 
-CREATE TABLE studios(
-  studio_id SERIAL UNIQUE,
+CREATE TABLE studios( -- view
+  id SERIAL UNIQUE,
   studio_name varchar(255) NOT NULL,
   country varchar(255) NOT NULL,
-  PRIMARY KEY (studio_id)
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE sponsors(
+CREATE TABLE sponsors( -- model
+  id SERIAL UNIQUE,
   studio_id int,
   movie_id int,
+  PRIMARY KEY (id),
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE,
   FOREIGN KEY (studio_id) REFERENCES studios(studio_id) ON DELETE CASCADE
 );
