e24f0752f042897101d47975e403f4f62bb4c4e9
qufeichen <qufei.chen@gmail.com>
2016-04-04 00:06:16 -0400

    added password hashing and confirmation
    password now saved as a hashed value, and password confirmation is now
    needed when choosing a password

--- mov_rec/app/models/user.rb
+++ mov_rec/app/models/user.rb
@@ -4,14 +4,14 @@ class User < ActiveRecord::Base
 	# one user can have many movie watches
 	has_many :movie_rating
 
-	# to create a new user, must have a password length > 8
-	validates :password, presence: true,
-			length: {minimum: 8}
-
 	# must enter a email with proper format
 	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   	validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+    # ensure password is secure (hashed) and length > 6
+	has_secure_password
+	validates :password, presence: true, length: {minimum: 6}
 end
