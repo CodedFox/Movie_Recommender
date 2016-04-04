8f44bfe8dc1f689efb4a2eeb8a88b8b0f3d1b996
qufeichen <qufei.chen@gmail.com>
2016-04-03 22:38:25 -0400

    updated params of users and profiles controllers

--- mov_rec/app/controllers/users_controller.rb
+++ mov_rec/app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :user_type_id, :date_of_birth)
+      params.require(:user).permit(:email, :password, :user_type_id)
     end
 end
