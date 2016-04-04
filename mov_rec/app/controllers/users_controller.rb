e93b68ed6ff031198d7a77b798192e521bb429f7
qufeichen <qufei.chen@gmail.com>
2016-04-04 18:14:41 -0400

    when editing user, change field title to 'New password'

--- mov_rec/app/controllers/users_controller.rb
+++ mov_rec/app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        flash[:success] = "Welcome to MovRec!"
         format.html {redirect_to "/profiles/new?userid=#{@user.id}"}
         # format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
