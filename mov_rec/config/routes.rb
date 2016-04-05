dba1c86b3ba46c8350ba26a0afed097c1e026e82
qufeichen <qufei.chen@gmail.com>
2016-04-05 15:27:39 -0400

    changed home page to just show login
    removed nav bar if not logged in

--- mov_rec/config/routes.rb
+++ mov_rec/config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   get "logout" => "sessions#destroy"
-  root "pages#login"
+  root "sessions#new"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
