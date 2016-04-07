514ee35d697f55763163797bd7a42c4aec4ce166
Diane Vu <dianevu@hotmail.ca>
2016-04-07 05:28:08 -0400

    WIP diverse raters in each genre
    max/min movies issue still there

--- mov_rec/app/models/movie_rating.rb
+++ mov_rec/app/models/movie_rating.rb
@@ -1,4 +1,6 @@
 class MovieRating < ActiveRecord::Base
 	belongs_to :profile
 	belongs_to :movie
+
+	has_many :movie_topics, through: :movie
 end
--- mov_rec/app/models/movie_topic.rb
+++ mov_rec/app/models/movie_topic.rb
@@ -1,4 +1,6 @@
 class MovieTopic < ActiveRecord::Base
 	belongs_to :movie
 	belongs_to :topic
+
+	has_many :movie_ratings, through: :movie
 end
