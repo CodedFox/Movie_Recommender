2def31b9efa45e32ce96d5cdf92d8535e67580d1
Diane Vu <dianevu@hotmail.ca>
2016-04-07 16:47:10 -0400

    displaying movies in director/id!

--- mov_rec/app/models/director.rb
+++ mov_rec/app/models/director.rb
@@ -1,3 +1,5 @@
 class Director < ActiveRecord::Base
 	validates :first_name, presence:true
+
+	has_many :directs
 end
--- mov_rec/app/models/movie.rb
+++ mov_rec/app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ActiveRecord::Base
 	has_many :movie_topics
 	has_many :movie_ratings
 	has_many :sponsors
+	has_many :directs
 
 	validates :movie_name, presence:true
 end
