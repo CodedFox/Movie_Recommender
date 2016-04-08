5a548a63b503b08578a49b16d790cdc089fe56b4
qufeichen <qufei.chen@gmail.com>
2016-04-08 19:29:51 -0400

    updated models associations for used_devices

--- mov_rec/app/models/device.rb
+++ mov_rec/app/models/device.rb
@@ -1,3 +1,4 @@
 class Device < ActiveRecord::Base
 	has_many :used_devices
+	has_many :profiles, through: :used_devices
 end
--- mov_rec/app/models/profile.rb
+++ mov_rec/app/models/profile.rb
@@ -4,4 +4,7 @@ class Profile < ActiveRecord::Base
 	has_many :likes_topics
 	has_many :topics, through: :likes_topics
 	has_many :movie_ratings
+
+	has_many :used_devices
+	has_many :devices, through: :used_devices
 end
