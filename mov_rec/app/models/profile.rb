acc2c026a6e2babed22df85ee5f3090d217d1ad8
qufeichen <qufei.chen@gmail.com>
2016-04-08 17:34:34 -0400

    added likes_topics checkboxes to profile

--- mov_rec/app/models/profile.rb
+++ mov_rec/app/models/profile.rb
@@ -1,8 +1,7 @@
 class Profile < ActiveRecord::Base
-	# each profile belongs to a user
 	belongs_to :user
-	# each profile can have many topics liked
+
 	has_many :likes_topics
-	# each profile can have many topics liked
+	has_many :topics, through: :likes_topics
 	has_many :movie_ratings
 end
--- mov_rec/app/models/topic.rb
+++ mov_rec/app/models/topic.rb
@@ -2,6 +2,7 @@ class Topic < ActiveRecord::Base
 	has_many :movie_topics
 	has_many :movies, through: :movie_topics
 	has_many :likes_topics
+	has_many :profiles, through: :likes_topics
 
 def self.search(search)
 	Topic.where('lower(genre_name) like lower(:value)', value: "%#{search}%" )
