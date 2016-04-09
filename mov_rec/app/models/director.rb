b17be91a719c33683cfe01cf03fb646accf5c100
Diane Vu <dianevu@hotmail.ca>
2016-04-09 17:58:51 -0400

    director&studios our featured actor worked with!
    on home page!

--- mov_rec/app/models/director.rb
+++ mov_rec/app/models/director.rb
@@ -2,6 +2,8 @@ class Director < ActiveRecord::Base
 	validates :first_name, presence:true
 
 	has_many :directs
+	has_many :movies, through: :directs
+	has_many :movie_casts, through: :movies
 
 def self.search(search)
 	Director.where("lower(first_name)|| ' '|| lower(last_name) like lower(:value)",value: "%#{search}%" )
--- mov_rec/app/models/studio.rb
+++ mov_rec/app/models/studio.rb
@@ -1,6 +1,9 @@
 class Studio < ActiveRecord::Base
 	has_many :sponsors
 
+	has_many :movies, through: :sponsors
+	has_many :movie_casts, through: :movies
+
 def self.search(search)
 	Studio.where('lower(studio_name) like lower(:value)', value: "%#{search}%" )
 end
